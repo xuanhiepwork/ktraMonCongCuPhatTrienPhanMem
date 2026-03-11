297f7022697e8b52e02ad3e08904d43ec75339e7
Froggg <hoangsh59@gmail.com>
2026-03-11 18:20:23 +0700

    Delete file

--- src/index.js
+++ src/index.js
@@ -11,19 +11,7 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
-// 2. Route xử lý đăng nhập (đổi thành tiếng Việt)
-app.post('/dang-nhap', (req, res) => {
-    const { txtUserName, txtpassword } = req.body;
-    console.log(`Tiến trình xử lý đăng nhập cho: ${txtUserName}`);
 
-    // Kiểm tra tài khoản admin/admin
-    if (txtUserName === 'admin' && txtpassword === 'admin') {
-        console.log("Xác thực thành công. Chuyển hướng...");
-        res.sendFile(path.join(__dirname, '../views/success.html'));
-    } else {
-        res.status(401).send('Tài khoản hoặc mật khẩu không chính xác!');
-    }
-});
 
 app.listen(PORT, () => {
     console.log(`Server đang chạy tại http://localhost:${PORT}`);
