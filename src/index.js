dda82261d468f0350fd88fffef4f89cc56096177
xuanhiep <xuanhiep.work@gmail.com>
2026-03-11 10:01:37 +0700

    tạo project

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
-res.sendFile(path.join(__dirname, '../views/index.html'));
-app.use(express.static('public'));
-
 const express = require('express');
+const path = require('path'); // Cần thiết để xử lý đường dẫn file
 const app = express();
 const PORT = 3000;
 
+// 1. Cấu hình phục vụ file tĩnh (CSS, Images, JS của frontend)
+// Nó sẽ tìm trong thư mục 'public'
+app.use(express.static(path.join(__dirname, '../public')));
+
+// 2. Định nghĩa Route chính để trả về file index.html
 app.get('/', (req, res) => {
-    res.send('Chào mừng bạn đến với dự án Node.js!');
+    // __dirname là thư mục chứa file index.js (tức là thư mục src)
+    // '../views/index.html' giúp đi ra ngoài src rồi vào views
+    res.sendFile(path.join(__dirname, '../views/index.html'));
 });
 
+// 3. Khởi chạy Server
 app.listen(PORT, () => {
     console.log(`Server đang chạy tại http://localhost:${PORT}`);
 });
